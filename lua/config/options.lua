d133dc89a7531cfa05c8d81edc4c483056656dbd
muh1uw <muh1uw@cs.washington.edu>
2024-05-21 18:57:17 -0700

    added flatten plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -93,7 +93,7 @@ vim.o.guifont = 'RobotoMono Nerd Font:h14'
 vim.g.neovide_transparency = 0.8
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_cursor_animation_length = 0.06
-vim.g.neovide_scroll_animation_length = 0.15
+vim.g.neovide_scroll_animation_length = 0.20
 
 if vim.g.neovide then
   vim.keymap.set('v', '<C-S-c>', '"+y') -- Copy
