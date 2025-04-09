b7eba9badc7736530d9afd811698299e4e1188b0
tybuu <tybuu1234@gmail.com>
2025-04-08 18:12:02 -0700

    removed codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    opts = {
-      wrapper = {
-        language_server = "home/tybuu/.nix-profile/bin/codeium_language_server",
-      },
-    },
-  },
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   opts = {
+  --     wrapper = {
+  --       language_server = "home/tybuu/.nix-profile/bin/codeium_language_server",
+  --     },
+  --   },
+  -- },
 }
